89773e2641131b1f41f8f0dc872d4e6c8f105145
ctolon@hotmail.com <ctolon@hotmail.com>
2016-03-10 16:42:37 +0100

    poblar

---
+++ mysql/poblar/insercionIngredientes.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class insercionIngredientes{
+
+	public static void main (String [] args) {
+		try{
+			Scanner br = new Scanner(new File("/home/carlos/GIT/ME4L/mysql/ingredientes.txt"));
+			String tipo = "Otros";
+			while(br.hasNextLine()){
+				String linea = br.nextLine();
+				if (linea.substring(0,1).equals("-")) {
+					tipo = linea.substring(1, linea.length());
+				}else{
+					linea = linea.trim();
+					System.out.println("INSERT INTO ingrediente VALUES ('" + linea + "', '" + tipo + "');");
+					
+				}
+				
+			}
+		}catch(FileNotFoundException e){
+
+		}
+
+	}
+
+
+}
---
+++ mysql/poblar/recetas.sql
@@ -0,0 +1,27 @@
+INSERT INTO receta VALUES (1,'Judias verdes','Quitarle las dos puntas y partir por la mitad las judias, pelar tantas patatas como se crea conveniente.
+	Escurrir tanto judias como patatas para limpiar la suciedad.
+	Poner a hervir el agua añadiendo poco mas de media cucharada de sal.
+	Una vez el agua este hirviendo echar las judias y a las patatas.
+	Esperar a que el agua vuelva a hervir, entonces contamos media hora.
+	Las retiramos del fuego y las servimos en platos echando el aceite que se crea conveniente.
+	Y ya estan listas para comer',
+	'primero', 4);
+-- Dos tazas de agua por una de arroz
+INSERT INTO receta VALUES (2, 'Arroz blanco', 'En una cacerola ponemos una cucharada de aceite, 
+	calentamos y sofreímos ligeramente un diente de ajo entero, con la piel incluida.
+	Echamos el arroz en la cacerola y una cucharadita de sal, y removemos bien hasta que el arroz se empape con el aceite.
+	Entonces echamos las 2 tazas de agua y ponemos el fuego vivo para que empiece a hervir.
+	Una vez hierva bajamos a fuego lento y dejamos que hierva durante 20 minutos.
+	Probamos pasado el tiempo (se debería haber consumido el agua ya) y comprobamos si el arroz está en su punto o necesita unos minutos más, 
+	aunque en ese momento si lo retiráis del fuego y dejáis que repose se terminará de cocer.',
+	'primero',1);
+INSERT INTO receta VALUES (3, 'Huevo frito', 'Echamos buena cantidad de aceite en una sarten, esperamos que se caliente.
+	Con cuidado cascamos el huevo y lo echamos rapido sobre la sarten, nos podemos ayudar de un vaso para que el aceite no salte,
+	es decir, cascamos el huevo en el vaso y luego lo volcamos. Una vez en la sarten le echamos una pizca de sal y le vamos echando aceite por encima para que se fria.
+	Cuando la yema esta en su punto lo sacamos con una rasera lo dejamos escurrir para que no manche y ya lo tenemos listo.',
+	'indiferente',1);
+INSERT INTO receta VALUES (4, 'Salchichas con salsa', 'Colocamos una capa de aceite sobre una sarten de tamaño adecuado para el de las salchichas.
+	Dejamos calentar el aceite, una vez este caliente echamos las salchichas, les vamos dando vueltas para que toda la salchicha se cocine
+	uniformemente, podemos ir pinchando con un cuchillo para comprobar si se ha hecho por dentro.
+	Escurrir las salchichas antes de depositar en un plato y acompañarlas con tomate, ketchup u otra salsa al gusto',
+	'segundo',1);
