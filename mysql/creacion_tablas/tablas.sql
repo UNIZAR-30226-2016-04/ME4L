04a4c4e43809e309aa264e8e51cb46d7965c4622
ctolon@hotmail.com <ctolon@hotmail.com>
2016-03-29 17:29:03 +0200

    TABLAS

--- mysql/creacion_tablas/tablas.sql
+++ mysql/creacion_tablas/tablas.sql
@@ -10,15 +10,15 @@ CREATE TABLE receta (
 
 CREATE TABLE ingrediente (
 	Nombre varchar(20),
-	TipoIngrediente varchar(15) not null,
+	TipoIngrediente varchar(25) not null,
 	Primary Key (nombre)
 );
 
 CREATE TABLE comentarios (
 	idReceta int(4),
-	idComentario int(4) auto_increment,
+	idComentario int(4) ,
 	comentario varchar(140) not null,
-	Foreign Key(idReceta) REFERENCES id (receta) ON DELETE CASCADE,
+	Foreign Key(idReceta) REFERENCES receta (id) ON DELETE CASCADE,
 	Primary Key (idReceta, idComentario)
 );
 
@@ -26,7 +26,7 @@ CREATE TABLE puntuacion (
 	idReceta int(4),
 	ip varchar(30) not null,
 	puntos int(2),
-	Foreign Key(idReceta) REFERENCES id (receta) ON DELETE CASCADE,
+	Foreign Key(idReceta) REFERENCES receta (id) ON DELETE CASCADE,
 	Primary Key(ip, idReceta)
 );
 
@@ -35,7 +35,7 @@ CREATE TABLE componente (
 	ingrediente varchar(20),
 	esPrincipal int(1),
 	peso int(4) not null,
-	Foreign Key(idReceta) REFERENCES id (receta) ON DELETE CASCADE,
+	Foreign Key(idReceta) REFERENCES receta (id) ON DELETE CASCADE,
 	Foreign Key(ingrediente) REFERENCES Nombre (ingrediente) ON DELETE CASCADE,
 	Primary Key (idReceta, ingrediente)
 );
