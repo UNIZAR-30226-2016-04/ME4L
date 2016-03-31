0c086f76e2ebf75534bc73a9b3206adbc8352903
Pablo Viñuales <679609@unizar.es>
2016-03-31 20:48:29 +0200

    Actualizado para funcionar en la raspberry

--- Codigo/app/Conexion.java
+++ Codigo/app/Conexion.java
@@ -6,9 +6,12 @@ import java.sql.SQLException;
 
 public class Conexion {
 
-	private static String servidor = "jdbc:mysql://127.0.0.1:3306/me4l";
-	private static String user = "proySoftware";
-	private static String pass = "me4l1234";
+//	private static String servidor = "jdbc:mysql://127.0.0.1:3306/me4l";
+//	private static String user = "proySoftware";
+//	private static String pass = "me4l1234";
+	private static String servidor = "jdbc:mysql://81.32.239.119:8100/meal";
+	private static String user = "root";
+	private static String pass = "root";
 	private static String driver = "com.mysql.jdbc.Driver";
 	private static Connection conexion;
 
